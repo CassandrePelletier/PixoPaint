df43e48fd6b77f871b9b46f1b655c5838ef69b10
Cassandre Pelletier <CAPEL208@ulaval.ca>
2025-01-02 00:54:31 -0500

    basics of the grid

--- src/main/java/Main/CanvasController.java
+++ src/main/java/Main/CanvasController.java
@@ -32,7 +32,18 @@ public class CanvasController {
         int width = (int)dimensionsCanvas.getWidth();
         int height = (int)dimensionsCanvas.getHeight();
 
-        // Dessine une ligne : coordonnées de départ (x1, y1) et de fin (x2, y2)
-        graphicsContext.strokeLine(0, 0, width, height);
+        initializeGrid(width, height);
+    }
+
+    public void initializeGrid(int width, int height){
+        // +10 to be able to see individual lines (temporary)
+        // TODO : zoom and scale-to-screen with fewer pixels
+        for (int i = 0; i <= width; i+=10) {
+            for (int j = 0; j <= height; j+=10) {
+                // Start (x1, y1) and finish (x2, y2)
+                graphicsContext.strokeLine(i, 0, i, height);
+                graphicsContext.strokeLine(0, j, width, j);
+            }
+        }
     }
 }
